93358c9a2db73994f375fbd491115c86fc66ab12
Fabricio Soares <fabriciosoares.pr@gmail.com>
2020-03-30 00:55:30 -0300

    Validação de rotas com Celebrate

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const { errors } = require('celebrate');
 const routes = require('./routes');
 
 const app = express();
@@ -7,5 +8,6 @@ const app = express();
 app.use(cors());
 app.use(express.json()); // Para que a aplicação entenda os objetos em formato Json
 app.use(routes);
+app.use(errors());
 
 app.listen(3333);
